215bd5f82c4afbde652eae28f8f1e19e945b05a0
OG <ogematribble@gmail.com>
2026-04-14 17:57:01 -0500

    yes.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,8 +3,8 @@ fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 lua54 'yes'
-author 'BCC Team'
-description 'bcc-farming'
+author 'Dark Scripts'
+description 'dark-farming'
 
 shared_scripts {
     'configs/*.lua',
